6ceadbcbfb97aca8cd82bcaba6ef70e85b9f7b46
oburnett127 <oburnett127@gmail.com>
2020-05-23 16:54:48 -0500

    first commit

---
+++ accounts-microservice/src/main/java/com/example/account/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.example.account;
+
+import java.util.List;
+
+public interface AccountRepository {
+	
+	List<Account> getAllAccounts();
+	
+	Account getAccount(String number);
+}
---
+++ accounts-microservice/src/main/java/com/example/account/StubAccountRepository.java
@@ -0,0 +1,36 @@
+package com.example.account;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StubAccountRepository implements AccountRepository {
+	
+	private Map<String, Account> accountsByNumber = new HashMap<String, Account>();
+	
+	public StubAccountRepository() {
+		Account account = new Account(1000l, "Frank" , "5115");
+		accountsByNumber.put("5115", account);
+		account = new Account(2000l, "Edward" , "2089");
+		accountsByNumber.put("2089", account);
+		account = new Account(3000l, "William" , "1286");
+		accountsByNumber.put("1286", account);
+		Logger.getLogger(StubAccountRepository.class).info("Created StubAccountRepository");
+	}
+	
+	@Override
+	public List<Account> getAllAccounts() {
+		return new ArrayList<Account>(accountsByNumber.values());
+	}
+
+	@Override
+	public Account getAccount(String number) {
+		return accountsByNumber.get(number);
+	}
+
+}
